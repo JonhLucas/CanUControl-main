b9831f2de610eb2b4331a32fabb9e4ae994512e6
JonhLucas <38635930+JonhLucas@users.noreply.github.com>
2023-05-22 23:19:18 -0300

    Add files via upload

--- mycontroller.py
+++ mycontroller.py
@@ -25,7 +25,8 @@ class Control:
 		self.referenceError = 0
 		self.lastResetTime = time.time()
 		self.moving = True
-		self.counter = 0
+		self.right = False
+		self.left = False
 
 	def step(self, received):
 
@@ -35,7 +36,6 @@ class Control:
 		
 		if received:
 			try:
-				
 					
 				land, level = received[1], received[2]
 				character_x, character_y, target_x, target_y = received[3], received[4], received[5], received[6]
@@ -71,31 +71,43 @@ class Control:
 						T = 0.7
 				elif land == 2:
 					if level == 1.0:
+
 						bins = np.array([-np.pi * 3/8, -np.pi/8 - 0.03, np.pi/8 + 0.03, np.pi * 3/8])
 						inds_target = np.digitize(target_x, bins)
 						inds_character = np.digitize(character_x, bins)
 						print(inds_target)
-						if inds_character < inds_target:
+						if inds_character < inds_target and not self.left:
 							print('direita', target_x, character_x, target_x - character_x, target_y)
 							if character_y < 0.55 and self.moving:
+								self.right = True
 								return -1
-							elif np.abs(target_x - character_x) > 0.05:
+							#elif np.abs(target_x - character_x) > 0.05:
+							else:
 								self.moving = False
 								return 1
-							
-						elif inds_character > inds_target:
+						elif inds_character > inds_target and not self.right:
 							print('Esquerda', target_x, character_x, target_x - character_x, character_y)
 							if character_y > -0.55 and self.moving:
+								self.left = True
 								return 1
-							elif np.abs(target_x - character_x) > 0.05:
+							#elif np.abs(target_x - character_x) > 0.05:
+							else:
 								self.moving = False
 								return -1
+						elif np.abs(character_y) > 0.5 and not self.moving:
+							print('ajustando', character_y)
+							return 0
 						else:
 							self.moving = True
+							self.right = False
+							self.left = False
 							return 0
 							
-						
 					elif level == 2.0:
+						self.moving = True
+						self.right = False
+						self.left = False
+						
 						error = r - x
 						K = np.array([0.7599217,   0.6542919])
 						v = (K @ error) * 3
@@ -104,9 +116,11 @@ class Control:
 						print(u, error)
 						return controlSignal
 					elif level == 3.0:
+						if r[0] > 0.4:
+							r[0] = 0
+							r[1] = 0
 						error = r - x
 						K = np.array([1.2372688,   0.1769447])
-						
 						v = (K @ error) * 4
 						print(K, error, v)
 						controlSignal = np.clip(v, -1, 0.7)[0]
@@ -147,7 +161,7 @@ class Control:
 						if(x[0] < 0.0):
 							E = np.array([[-1], [0]])				
 						self.referenceError = (r-x) * 0.03
-						Ke = 1
+						Ke = 10
 						T = 0.7
 						
 					if(level == 2):
@@ -155,7 +169,7 @@ class Control:
 						if(x[0] < 0.0):
 							K = np.array([10.1258, -10.4941])		
 						self.referenceError = (r-x) * 0.03
-						Ke = 1
+						Ke = 10
 						T = 0.7
 					
 					if(level == 3):
@@ -166,22 +180,22 @@ class Control:
 							E = np.array([[0], [-1]])		
 						self.referenceError = (r-x) * 0.03
 						Ke = 1
-						T = 0.7
+						T = 0.15
 						
 					if(level == 4):
 						K = np.array([13.0014, -14.4652])
 						if(-0.1582*x[0] + 1.8467*x[1] < 0.0): 
 							K = np.array([-13.0014, 14.4652])	
 						self.referenceError = (r-x) * 0.03
-						Ke = 1
-						T = 0.7
+						Ke = 10
+						T = 1.5
 				
 				
 				else:
 					print("\nFora de alcance\n")
 					controlSignal = self.lastControlSignalSent
 					
-				u = K @ (self.referenceError)
+				u = Ke * K @ (self.referenceError)
 				s = u
 
 				controlSignal = np.clip(s, -1, 1)[0]
